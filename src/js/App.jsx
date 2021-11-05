5bc4742b17fb38ce8f8c6f15006095fed4b5d7ee
Josiah <54999121+josiahjswab@users.noreply.github.com>
2021-11-04 20:28:12 -0700

    Working Comparision

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import SortAndCompare from './SortAndCompareList/SortAndCompare';
 
 export default function App() {
     return (
         <div>
-            Hello Passion.
+            <p>This was made to compare lists from the watch table when debugging in Visual Studios. It is using the quotation marks around values to split the string into an array. If you manually add any values please make sure to surround it with (") until I make it so it can handle multiple ways of entry.</p>
+            <SortAndCompare/>
         </div>
     )
 }
--- src/js/SortAndCompareList/SortAndCompare.jsx
+++ src/js/SortAndCompareList/SortAndCompare.jsx
@@ -1,44 +1,85 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { utilityForEachLi, formatListIntoArray } from '../common';
-// import { testList } from './testList.js'
-// import { dbList } from './dbList.js'
+import './sortAndCompare.css';
 
 function SortAndCompare() {
-    const [_listA, set_listA] = useState("");
-    const [_listB, set_listB] = useState("");
+    const [_listA, setListA] = useState(`
+    "A1000"
+    " Batman"
+    " CCEMerge"
+    " DASVID"
+    " Hulk"
+    " Iron Man"
+    " LN01"
+    " LN02"
+    " MergeToPH"
+    `);
+    const [_listB, setListB] = useState(`
+    "MergeToPH"
+    "LN01"
+    "LN02"
+    "Batman"
+    "Hulk"
+    "Iron Man"
+    "Spiderman"
+    "Superman"
+    "VID44"
+    "A100"
+    `);
+    const [dbIsMissing, setDbIsMissing] = useState([]);
+    const [testIsMissing, setTestIsMissing] = useState([]);
+    const [countAB, setCountAB] = useState([0, 0]);
+    const [resultA, setResultA] = useState("");
+    const [resultB, setResultB] = useState("");
+    
+    function thisFoo() {
+        setDbIsMissing([]);
+        setTestIsMissing([]);
+        let listA = formatListIntoArray(_listA);
+        let listB = formatListIntoArray(_listB);
 
-    let listA = formatListIntoArray(_listA);
-    let listB = formatListIntoArray(_listB);
+        setCountAB((countAB) => [listA.length, countAB[1]]);
+        listA.map((item, index) => {
+            if (!listB.includes(item)) {
+                setDbIsMissing((dbIsMissing) => [...dbIsMissing, item]);
+            }
+        });
+        
+        setCountAB((countAB) => [countAB[0], listB.length]);
+        listB.map((item, index) => {
+            if (!listA.includes(item)) {
+                setTestIsMissing((testIsMissing) => [...testIsMissing, item]);
+            }
+        });
 
-    let dbIsMissing =[];
-    let testIsMissing =[];
-    listA.map((item) => {
-        if (!listB.includes(item)) {
-            dbIsMissing.push(item);
-        }
-    });
-
-    listB.map((item) => {
-        if (!listA.includes(item)) {
-            testIsMissing.push(item);
-        }
-    });;
-    if (dbIsMissing.length > 0) {
-        // utilityForEachCLOG(dbIsMissing);
-        utilityForEachLi(dbIsMissing, ul1, `listA: ${dbIsMissing.length} out of ${listA.length} are not in the other list.`);
     }
     
-    if (testIsMissing.length > 0) {
-        // utilityForEachCLOG(testIsMissing);
-        utilityForEachLi(testIsMissing, ul2, `listB: ${testIsMissing.length} out of ${listB.length} are not in the other list.`);
-    
-    }
+    useEffect(() => {
+        setResultA(`List A: ${dbIsMissing.length} out of ${countAB[0]} are not in the other list.`);
+        setResultB(`List B: ${testIsMissing.length} out of ${countAB[1]} are not in the other list.`);
+    }, [dbIsMissing]);
+
     return (
-        <div>
-            <ul>list a
-            </ul>
-            <ul>list b
-            </ul>
+        <div id="container_sort_compare">
+            <div>
+                <textarea rows="20" type="textbox" value={_listA} onChange={(e) => setListA(e.target.value)} placeholder="List A..." />
+                <h2>{resultA}</h2>
+                <ol>
+                    {dbIsMissing.length > 0 ? dbIsMissing.map((item) => (
+                        <li>{item}</li>
+                    )): null}
+                </ol>
+            </div>
+            <button onClick={thisFoo} >Execute</button>
+            <div>
+                <textarea rows="20" type="textbox" value={_listB} onChange={(e) => setListB(e.target.value)} placeholder="List B..." />
+                <h2>{resultB}</h2>
+                <ol>
+                    {testIsMissing.length > 0 ? testIsMissing.map((item, index) => (
+                        <li key={item + index} >{item}</li>
+                    )): null}
+                </ol>
+            </div>
         </div>
     );
 }
