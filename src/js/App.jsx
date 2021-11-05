c9ba7d7b74761e9ed10c04509f49d045ab98ad15
Josiah <54999121+josiahjswab@users.noreply.github.com>
2021-11-04 21:50:51 -0700

    Adds important emojis

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -10,6 +10,7 @@ export default function App() {
             {showModal ? 
             <div id="iceout">
                 <div id='modal' >
+                    <h1>📝🔥✌️</h1>
                     <p>This was made to compare lists from the watch table when debugging in Visual Studios. <br/>It is using the quotation marks around values to split the string into an array. <br/>If you manually add any values please make sure to surround it with (") until I make it so it can handle multiple ways of entry.</p>
                     <button onClick={() => setShowModal(!showModal)}>X</button>
                 </div>
