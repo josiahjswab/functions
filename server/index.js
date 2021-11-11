bee45bae1f1f4e2cc4d656721ca09df5aa851b7a
Josiah <54999121+josiahjswab@users.noreply.github.com>
2021-11-11 14:46:41 -0800

    Adds Mongo connector

--- server/index.js
+++ server/index.js
@@ -2,16 +2,38 @@ const express = require('express')
 const app = express()
 var path = require('path');
 const port = 3003
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+// or as an es module:
+// import { MongoClient } from 'mongodb'
 
+// Connection URL
+const url = process.env.MONGODB_CONNECTION_STRING;
+const client = new MongoClient(url);
 
-// app.use(express.static('dist'))
+// Database Name
+const dbName = 'myProject';
 
-app.get('/hello', (req, res) => {
-  res.send('Hello World!')
-})
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('documents');
+
+  // the following code examples can be pasted here...
+
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
+
+app.use(express.static('dist'))
 
 app.get('*', (req, res) => {
-    console.log(path.resolve('../dist/index.html'));
     res.sendFile(path.resolve('./dist/index.html'));
   });
   
